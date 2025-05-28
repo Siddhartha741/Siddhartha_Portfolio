eb27c890141f99c16b5277d9ffab582b37257258
siddhartha namilikonda <siddartha.namilikonda@gmail.com>
2025-05-28 21:38:24 +0530

    Updates

---
+++ precache-manifest.a6bbc6ec774decc0497e4520f7081a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c587c1ee71e63e1ac389933c6bd797a0",
+    "url": "/Siddhartha_Portfolio/index.html"
+  },
+  {
+    "revision": "eadcfe5adabaf514a567",
+    "url": "/Siddhartha_Portfolio/static/css/main.22718620.chunk.css"
+  },
+  {
+    "revision": "953251822c99cf2338f9",
+    "url": "/Siddhartha_Portfolio/static/js/2.b3ba0595.chunk.js"
+  },
+  {
+    "revision": "eadcfe5adabaf514a567",
+    "url": "/Siddhartha_Portfolio/static/js/main.052a8ef3.chunk.js"
+  },
+  {
+    "revision": "15f13be3ea4986a64ea2",
+    "url": "/Siddhartha_Portfolio/static/js/runtime~main.1e526efc.js"
+  },
+  {
+    "revision": "188e5ef687b04a652cbe39afee673f9d",
+    "url": "/Siddhartha_Portfolio/static/media/HeroImage.188e5ef6.jpg"
+  }
+]);
